8ce027adf811f7e4e4d1fb80bdade95faf164c71
Mirko <bicchierai.mirko.developer@gmail.com>
2025-01-02 16:37:42 +0100

    good

---
+++ create_graph.py
@@ -0,0 +1,43 @@
+import json
+import matplotlib.pyplot as plt
+
+def create_graph(path, title, x_label, y_label):
+
+    with open(path, 'r') as f:
+        data = json.load(f)
+
+    plt.figure(figsize=(10, 6))
+
+    for trace in data:
+        x = trace["x"]
+        y = trace["y"]
+        name = trace["name"]
+
+        if "NaN" in y:
+            y = [float(value) if value != "NaN" else None for value in y]
+            x, y = zip(*[(x_val, y_val) for x_val, y_val in zip(x, y) if y_val is not None])
+
+        # Plot each trace
+        plt.plot(x, y, label=name)
+
+    # Add labels, legend, and title
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
+if __name__ == '__main__':
+    file_path = "CometMlData/loss VS step_chart_data.json"
+    title_graph = "Loss Vs Epoch"
+    y_l = "Loss"
+    x_l = "Epochs"
+    create_graph(file_path, title_graph, x_l, y_l)
+
+
+
+
+
+
+
