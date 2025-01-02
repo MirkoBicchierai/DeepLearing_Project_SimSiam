6f046c4cb3c1fe0fdbe00193ae9efdd709a599e0
Mirko <bicchierai.mirko.developer@gmail.com>
2025-01-02 18:41:42 +0100

    fix scheduling

--- main.py
+++ main.py
@@ -10,9 +10,10 @@ from common import transformAug, transform, knn_validation
 
 
 def learning_rate_schedule(opt, init_lr, actual_epoch, max_epochs):
+    """Decay the learning rate based on schedule"""
     cur_lr = init_lr * 0.5 * (1. + math.cos(math.pi * actual_epoch / max_epochs))
     for param_group in opt.param_groups:
-        if 'fixed' in param_group and param_group['fixed']:
+        if param_group['fixed']:
             param_group['lr'] = init_lr
         else:
             param_group['lr'] = cur_lr
@@ -33,7 +34,7 @@ if __name__ == "__main__":
     momentum = 0.9
     weight_decay = 0.0001
     epochs = 200
-    test_step = 1
+    test_step = 2
 
     knn_k = 200
     knn_t = 0.1
@@ -62,12 +63,13 @@ if __name__ == "__main__":
                     {'params': model.projector.parameters(), 'fixed': False},
                     {'params': model.predictor.parameters(), 'fixed': True}]
 
-    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
+    optimizer = optim.SGD(optim_params, lr=lr, momentum=momentum, weight_decay=weight_decay)
     scaler = torch.amp.GradScaler()
 
     num_batches = len(train_loader)
     for epoch in tqdm(range(epochs), desc="Training"):
         model.train()
+        learning_rate_schedule(optimizer, lr, epoch, epochs)
         running_loss = 0
         all_normalized_outputs = []
         for images_aug1, images_aug2 in train_loader:
@@ -88,8 +90,6 @@ if __name__ == "__main__":
                     all_normalized_outputs.append(z1_norm)
                     z2_norm = z2 / z2.norm(dim=1, keepdim=True)
                     all_normalized_outputs.append(z2_norm)
-
-        learning_rate_schedule(optimizer,lr,epoch,epochs)
 
         epoch_loss = running_loss / num_batches
         print(f"Epoch: {epoch + 1}, Loss: {epoch_loss}")
