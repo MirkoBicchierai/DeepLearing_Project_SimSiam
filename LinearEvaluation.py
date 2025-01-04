3400754378cf5ff80a974eafa090fde5c2a14fc8
Mirko <bicchierai.mirko.developer@gmail.com>
2025-01-04 17:32:03 +0100

    Linear Evaluation refactor and add a parser

--- LinearEvaluation.py
+++ LinearEvaluation.py
@@ -1,11 +1,13 @@
 import comet_ml
 import torch
+import torchvision
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
 from DataLoader import ImageNetDataset
-from Model import LinearEvaluationModel
+from Model import LinearEvaluationModel, NetModel
 import torchvision.transforms as T
+import argparse
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -19,15 +21,12 @@ def adjust_learning_rate(optimizer, epoch, lr, schedule):
 
 def test(model, test_loader, criterion):
     model.eval()
-    total_top1, total_top5, total_num = 0.0, 0.0, 0
-    total_val_loss = 0.0
+    total_top1, total_top5, total_num = 0, 0, 0
+    total_val_loss = 0
 
     with torch.no_grad():
-        test_bar = tqdm(test_loader)
-        for images, target in test_bar:
-            images = images.cuda(non_blocking=True)
-            target = target.cuda(non_blocking=True)
-
+        for images, target in tqdm(test_loader):
+            images, target = images.cuda(non_blocking=True), target.cuda(non_blocking=True)
             output = model(images)
             loss = criterion(output, target)
 
@@ -37,65 +36,185 @@ def test(model, test_loader, criterion):
             pred = pred.t()  # transpose to shape [k, batch_size]
             correct = pred.eq(target.view(1, -1).expand_as(pred))
 
-            correct_top1 = correct[0].float().sum().item()  # just the first prediction
-            correct_top5 = correct[:5].float().sum().item()  # any of top 5 predictions
+            correct_top1 = correct[0].float().sum().item()
+            correct_top5 = correct[:5].float().sum().item()
             total_top1 += correct_top1
             total_top5 += correct_top5
-
             total_num += images.size(0)
 
     return (total_top1 / total_num), (total_top5 / total_num), total_val_loss / total_num
 
 
+def get_dataloader(dataset, batch_size, num_workers):
+    if dataset == "MiniImageNet":
+        num_classes = 100
+        train_transform = T.Compose(
+            [
+                T.RandomResizedCrop(224),
+                T.RandomHorizontalFlip(),
+                T.ToTensor(),
+                T.Normalize(
+                    mean=[0.485, 0.456, 0.406],  # Normalize with ImageNet mean
+                    std=[0.229, 0.224, 0.225], # Normalize with ImageNet std
+                ),
+            ]
+        )
+
+        test_transform = T.Compose(
+            [
+                T.Resize(256),
+                T.CenterCrop(224),
+                T.ToTensor(),
+                T.Normalize(
+                    mean=[0.485, 0.456, 0.406],  # Normalize with ImageNet mean
+                    std=[0.229, 0.224, 0.225], # Normalize with ImageNet std
+                ),
+            ]
+        )
+
+        test_set = ImageNetDataset(
+            root_dir="./Dataset/SPLITTED/Test/", mode="eval", transform=test_transform
+        )
+
+        test_loader = DataLoader(
+            test_set,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+        train_set = ImageNetDataset(
+            root_dir="./Dataset/SPLITTED/Train/", mode="eval", transform=train_transform
+        )
+        dataset_length = len(train_set)
+        train_size = int(0.8 * dataset_length)
+        val_size = dataset_length - train_size
+
+        # Split the dataset into training and validation sets
+        train_subset, val_subset = random_split(train_set, [train_size, val_size])
+
+        train_loader = DataLoader(
+            train_subset,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+        val_loader = DataLoader(
+            val_subset,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            pin_memory=True,
+        )
+
+    if dataset == "Cifar10":
+        num_classes = 10
+        transform_train = T.Compose(
+            [
+                T.RandomCrop(32, padding=4),
+                T.RandomHorizontalFlip(),
+                T.ToTensor(),
+                T.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616]),
+            ]
+        )
+
+        transform_test = T.Compose(
+            [
+                T.ToTensor(),
+                T.Normalize(mean=[0.4914, 0.4822, 0.4465], std=[0.2470, 0.2435, 0.2616]),
+            ]
+        )
+
+        train_set = torchvision.datasets.CIFAR10(
+            root="./Dataset/Cifar", train=True, download=True, transform=transform_train
+        )
+
+        test_set = torchvision.datasets.CIFAR10(
+            root="./Dataset/Cifar", train=False, download=True, transform=transform_test
+        )
+
+        train_set, val_set = random_split(train_set, [40000, 10000])
+
+        train_loader = torch.utils.data.DataLoader(
+            train_set,
+            batch_size=batch_size,
+            shuffle=True,
+            num_workers=num_workers,
+            drop_last=False,
+            pin_memory=True,
+        )
+
+        val_loader = torch.utils.data.DataLoader(
+            val_set,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            drop_last=False,
+            pin_memory=True,
+        )
+
+        test_loader = torch.utils.data.DataLoader(
+            test_set,
+            batch_size=batch_size,
+            shuffle=False,
+            num_workers=num_workers,
+            drop_last=False,
+            pin_memory=True,
+        )
+
+    return test_loader, val_loader, train_loader, num_classes
+
+def get_args():
+
+    parser = argparse.ArgumentParser(description="Parser for training parameters")
+
+    parser.add_argument("--path", type=str, default="Models/SimSiam/BUONO_40.pth",
+                        help="Path to the model file")
+    parser.add_argument("--batch_size", type=int, default=128,
+                        help="Batch size for training")
+    parser.add_argument("--epochs", type=int, default=100,
+                        help="Number of epochs for training")
+    parser.add_argument("--lr", type=float, default=0.02,
+                        help="Learning rate for optimizer")
+    parser.add_argument("--momentum", type=float, default=0.9,
+                        help="Momentum value for optimizer")
+    parser.add_argument("--weight_decay", type=float, default=0.0,
+                        help="Weight decay (L2 regularization) value")
+    parser.add_argument("--num_workers", type=int, default=8,
+                        help="Number of workers for data loading")
+    parser.add_argument("--dataset", type=str, default="Cifar10",
+                        help="Dataset name (e.g., Cifar10, MiniImageNet)")
+
+    return parser.parse_args()
+
+
 def main():
 
-    path = "Models/SimSiam/model_200_96_Checkpoint_150.pth"
-    batch_size = 48
-    epochs = 100
-    lr = 0.1
-    momentum=0.9
-    weight_decay= 0
-    schedule = [60,80]
+    args = get_args()
+    path = args.path
+    batch_size = args.batch_size
+    epochs = args.epochs
+    lr = args.lr
+    momentum = args.momentum
+    weight_decay = args.weight_decay
+    num_workers = args.num_workers
+    dataset = args.dataset
+
+    schedule = [60, 80]
 
     comet_ml.login(api_key="S8bPmX5TXBAi6879L55Qp3eWW")
     exp = comet_ml.Experiment(project_name="Deep Learning Project", auto_metric_logging=False, auto_param_logging=False)
     parameters = {'batch_size': batch_size, 'learning_rate': lr, 'momentum': momentum, 'weight_decay': weight_decay}
     exp.log_parameters(parameters)
 
-    pretrained_model = torch.load(path)
-    pretrained_model.stop_grad = True
-    pretrained_model.to(device)
+    test_loader, val_loader, train_loader, num_classes = get_dataloader(dataset, batch_size, num_workers)
 
-    train_transform = T.Compose(
-        [
-            T.RandomResizedCrop(224),
-            T.RandomHorizontalFlip(),
-            T.ToTensor(),
-            T.Normalize(
-                mean=[0.485, 0.456, 0.406],  # Normalize with ImageNet mean
-                std=[0.229, 0.224, 0.225],
-            ),
-        ]
-    )
-    test_transform = T.Compose(
-        [
-            T.Resize(256),
-            T.CenterCrop(224),
-            T.ToTensor(),
-            T.Normalize(
-                mean=[0.485, 0.456, 0.406],  # Normalize with ImageNet mean
-                std=[0.229, 0.224, 0.225],
-            ),
-        ]
-    )
-
-    val_data = ImageNetDataset(root_dir="./Dataset/SPLITTED/Test/", mode="eval", transform=test_transform)
-    val_loader = DataLoader(val_data,batch_size=batch_size,shuffle=False,num_workers=8,pin_memory=True)
-
-    train_data = ImageNetDataset(root_dir="./Dataset/SPLITTED/Train/", mode="eval", transform=train_transform)
-    train_loader = DataLoader(train_data,batch_size=batch_size,shuffle=True,num_workers=8,pin_memory=True)
-
-    num_classes = train_data.num_classes
+    checkpoint = torch.load(path, map_location="cuda")
+    pretrained_model = NetModel(dim=512, predictor_dim=128,stop_grad=True).cuda()
+    pretrained_model.load_state_dict(checkpoint["model_state_dict"], strict=False)
 
     model = LinearEvaluationModel(input_dim=512, num_classes=num_classes, backbone=pretrained_model.backbone).cuda()
 
@@ -106,10 +225,10 @@ def main():
     model.eval()
     for epoch in tqdm(range(epochs)):
         adjust_learning_rate(optimizer, epoch, lr, schedule)
-        total_loss, total_num, train_bar = 0.0, 0, tqdm(train_loader)
-        for image, target in train_bar:
-            image = image.cuda(non_blocking=True)
-            target = target.cuda(non_blocking=True)
+        total_loss, total_num = 0, 0
+        for image, target in train_loader:
+            image = image.cuda()
+            target = target.cuda()
             output = model(image)
             loss = criterion(output, target)
 
@@ -122,13 +241,24 @@ def main():
 
         train_loss = total_loss / total_num
         print("Train Loss:", str(train_loss))
-        exp.log_metric('Linear Evaluation Loss Train', train_loss, step=epoch)
+        exp.log_metric(dataset + ' Linear Evaluation Loss Train', train_loss, step=epoch)
 
         top1, top5, val_loss = test(model, val_loader, criterion)
+
         print("Top1:", str(top1), "Top5:", str(top5), "Validation Loss:", str(val_loss))
-        exp.log_metric('Linear Evaluation Loss Evaluation', val_loss, step=epoch)
-        exp.log_metric('Linear Evaluation Top1 Accuracy', top1, step=epoch)
-        exp.log_metric('Linear Evaluation Top5 Accuracy', top5, step=epoch)
+        exp.log_metric(dataset + ' Linear Evaluation Loss Evaluation', val_loss, step=epoch)
+        exp.log_metric(dataset + ' Linear Evaluation Top1 Accuracy - Validation', top1, step=epoch)
+        exp.log_metric(dataset + ' Linear Evaluation Top5 Accuracy - Validation', top5, step=epoch)
+
+    top1, top5, _ = test(model, test_loader, criterion)
+    exp.log_metric(dataset + ' Linear Evaluation Top1 Accuracy - Test', top1)
+    exp.log_metric(dataset + ' Linear Evaluation Top5 Accuracy - Test', top5)
+    print("Accuracy Test - ", "Top1:", str(top1), "Top5:", str(top5))
+
+    torch.save({'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict()},
+               "Models/LinearEval/model_" + str(epochs) + "_" + str(batch_size) + "_" + dataset + ".pth")
+
 
 if __name__ == "__main__":
     main()
